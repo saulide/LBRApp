06864cbe1ea2d7a6d7beb9816e6b51e11a935e80
Saulius Archipovas <ich@sauli.de>
2011-02-15 18:51:24 +0000

    working slices

--- js/app.js
+++ js/app.js
@@ -11,8 +11,35 @@ new Ext.Application({
 // to active items write rootPanel.setActiveItem(number);
 // Defining items
 
+/*
+ * Handlers
+ */
+var tapHander = function(button, event) {
+	rootPanel.setActiveItem(main, {
+                type: 'slide',
+                reverse: true
+            });
+}
+var tapHanderTrue = function(button, event) {
+	rootPanel.setActiveItem(learningItemsFront, {
+                type: 'slide',
+            });
+}
+var tapHanderFalse = function(button, event) {
+	rootPanel.setActiveItem(learningItemsFront, {
+                type: 'slide',
+                reverse: true
+            });
+}
+var cardViewClickHandler = function(button, event) {
+		rootPanel.setActiveItem(main, {
+                type: 'slide',
+                reverse: true
+            });
+}
+
 var one = {
-	style: "background-color: #FFBF00, color:white",
+	style: "background: url('img/background.png')",
 	title: "one",
 	html: '<div id="lernWords"><img class="intro" src="img/learnWordsBig.png"/></div><div id="readTexts"><img class="intro" src="img/readTextBig.png"/></div>'
 	//html: '<img src="img/learnWordsBig.png"/><p><img src="img/learnWordsBig.png"/> ',
@@ -35,15 +62,116 @@ var four = {
 var multiple = {
 	
 }
-var alert = {
-	text : "Put as native",
-	ui : 'decline',
-}
 var carussel = new Ext.Carousel({
 	cardSwitchAnimation: 'slide',
 	fullscreen:true,
 	items: [three, four]
 });
+
+/*
+ * Here are the panels for the Learning views
+ */
+
+/*
+ * Choose card view
+ */
+// Items ( demo items)
+
+var backBtn = {
+	text: 'backs',
+	ui:'back',
+	handler: tapHander	
+}
+var learningItemsTrueBtn = {
+	text: 'I knew it',
+	ui:'confirm',
+	handler: tapHanderTrue		
+}
+var learningItemsFalseBtn = {
+	text: 'Repeat it',
+	ui:'decline',
+	handler: tapHanderFalse		
+}
+
+var backToolbar = {
+	xtype: 'toolbar',
+	ui:'light',
+	items: [backBtn],
+	dock: 'top'
+};
+var learningItemsBtn = {
+	xtype: 'toolbar',
+	ui:'light',
+	items: [learningItemsTrueBtn, learningItemsFalseBtn],
+	dock: 'bottom'
+};
+
+// learningItemsBtn.items = [learningItemsTrueBtn, learningItemsFalseBtn];
+
+var cardViewItem1 = {
+	style : 'background: url("img/background.png")',
+	title: "card",
+	html: '<div id="showCardCreate"><img class="intro" src="img/CardOverviewBackground.png"/></div><section><title>Labas</title><details>Keistas</details></section></div>',
+	handler: cardViewClickHandler
+};
+var cardViewItem2 = {
+	style : 'background: url("img/background.png")',
+	title: "card",
+	html: '<div id="showCardCreate"><img class="intro" src="img/CardOverviewBackground.png"/></div><section><title>Labas</title><details>Keistas</details></section></div>'
+};
+
+var learningItemsFront = {
+	style : 'background: url("img/background.png")',
+	title: "card",
+	html: '<div id="showCardCreate"><img class="intro" src="img/learningWordsBig.png"/></div><section><title>Labas</title><details>Keistas</details></section></div>'	
+}
+
+
+
+// Overview of the cards
+var learnPanel = new Ext.Panel({
+	fullscreen:true,
+	items: [learningItemsFront, learningItemsBtn],
+	dockedItems: [backToolbar,learningItemsBtn]
+});
+var cardOverview = new Ext.Carousel({
+	cardSwitchAnimation: 'slide',
+	items : [cardViewItem1,cardViewItem2],
+	dockedItems: [backToolbar],
+	listeners: {
+		click: {
+			element: 'body', //element: 'el', //bind to the underlying el property on the panel
+			fn: function(){
+				console.log('click el');
+				rootPanel.setActiveItem(learnPanel);
+			}
+		}
+	}
+});
+/*
+var cardOverview = new Ext.Carousel({
+	cardSwitchAnimation: 'slide',
+	items : [cardViewItem1,cardViewItem2],
+	dockedItems: [backToolbar, learningItemsTrueBtn, learningItemsFalseBtn],
+	listeners: {
+		click: {
+			element: 'el', //element: 'el', //bind to the underlying el property on the panel
+			fn: function(){
+				console.log('click el');
+			}
+		}
+	}
+});
+*/
+
+var main = new Ext.Panel({
+			fullscreen: true,
+			layout: 'card',
+			items: [one],
+			cardSwitchAnimation : 'slide',
+			reverse: true
+		});
+
 
 var rootPanel;
 
@@ -58,10 +186,11 @@ Ext.setup({
 		rootPanel = new Ext.Panel({
 			fullscreen: true,
 			layout: 'card',
-			items: [one, carussel]
+			items: [one]
 		});
+
 		$('#lernWords').live('click', function(){
-			rootPanel.setActiveItem(carussel);
+			rootPanel.setActiveItem(cardOverview);
 		});
 		if(!Ext.is.iPhone){
 			Ext.Msg.alert("Iphone", "This app is specially designed for iOS");
